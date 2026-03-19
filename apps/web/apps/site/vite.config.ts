3ea6489b63ffd48e6888a0bb7099aba719e2e666
Jacob Edley <edleyjacob@gmail.com>
2026-03-19 12:20:44 -0500

    move web apps to web directory

---
+++ apps/web/apps/site/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const sharedEnv = loadEnv(mode, resolve(__dirname, "../../.."), "");
+
+  for (const [key, value] of Object.entries(sharedEnv)) {
+    if (process.env[key] === undefined) {
+      process.env[key] = value;
+    }
+  }
+
+  return {
+    server: {
+      port: 3001
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+        "@rp/shared": resolve(__dirname, "../../shared/src")
+      },
+      dedupe: ["react", "react-dom", "react-router-dom"]
+    }
+  };
+});
