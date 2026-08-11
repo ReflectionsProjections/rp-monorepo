3ce3ba5be85125362fbc698f52c2d3d4770949fa
seventhriver <129530873+seventhriver@users.noreply.github.com>
2026-08-10 20:25:06 -0500

    commit oops

--- apps/web/app/sections/home/components/Home/Footer/Footer.tsx
+++ apps/web/app/sections/home/components/Home/Footer/Footer.tsx
@@ -1,26 +1,16 @@
-import {
-  Box,
-  Flex,
-  Grid,
-  Image,
-  Link,
-  Text,
-  useMediaQuery
-} from "@chakra-ui/react";
-
-import { Link as ChakraLink } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
+import { Flex, Grid, Image, Link, Text, useMediaQuery } from "@chakra-ui/react";
 
 const footerLinkIcons: { src: string; to: string }[] = [
+  { src: "email_icon.svg", to: "mailto:contact@reflectionsprojections.org" },
   {
-    src: "linkedin_red.svg",
+    src: "linkedin_icon.svg",
     to: "https://linkedin.com/company/reflections-projections-uiuc"
   },
-  { src: "instagram_red.svg", to: "https://instagram.com/uiuc_rp/" },
-  { src: "tiktok_red.svg", to: "https://www.tiktok.com/@uiuc_rp" },
-  { src: "facebook_red.svg", to: "https://facebook.com/acmrp/" },
-  { src: "github_red.svg", to: "https://github.com/ReflectionsProjections" },
-  { src: "email_red.svg", to: "mailto:contact@reflectionsprojections.org" }
+  { src: "facebook_icon.svg", to: "https://facebook.com/acmrp/" },
+
+  { src: "tiktok_icon.svg", to: "https://www.tiktok.com/@uiuc_rp" },
+  { src: "github_icon.svg", to: "https://github.com/ReflectionsProjections" },
+  { src: "instagram_icon.svg", to: "https://instagram.com/uiuc_rp/" }
 ]; // there should be <= 6 of these
 
 export const Footer = () => {
@@ -29,34 +19,57 @@ export const Footer = () => {
   return (
     <Flex
       w="100%"
+      h="fit-content"
       flexDir="column"
-      bgGradient="linear(to-b, #222020ff 0%, #100E0E 100%)"
+      bgGradient="linear(to-b, #0F062D 0%, #7C1493 100%)"
       alignItems="center"
+      position="relative"
     >
       <Flex
         w="100%"
         maxW="1500px"
-        alignItems="center"
-        justifyContent={{ base: "center", md: "space-between" }}
+        h="inherit"
         flexDir="row"
-        px={6}
-        py={10}
+        alignItems="flex-end"
+        justifyContent={{ base: "center", md: "space-between" }}
+        px={12}
+        py={14}
+        zIndex={2}
+        position="absolute"
+        bottom={0}
       >
-        <Image
-          display={{
-            base: "none",
-            md: "block"
-          }}
-          src="/site/footer/footer_car.svg"
-          zIndex={2}
-          maxH={{ md: "150px", lg: "200px" }}
-          transform="scaleX(-1)"
-        />
+        {/* left side content */}
         <Flex
           flexDirection="column"
           h="inherit"
-          pb={{ base: 0, md: "20px" }}
-          pr={{ base: 0, md: 10 }}
+          alignItems="flex-start"
+          justifyContent="flex-end"
+          zIndex={2}
+        >
+          <Image
+            display={{
+              base: "none",
+              md: "block"
+            }}
+            src="/site/footer/2026_footer_logo.svg"
+            maxH={{ md: "150px", lg: "200px" }}
+            mx={8}
+            my={16}
+          />
+          <Text
+            color="#FFFFFF57"
+            w="100%"
+            fontFamily="Inter"
+            fontSize={{ base: "md", md: "lg", lg: "xl" }}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            &copy;2026 by Reflections | Projections
+          </Text>
+        </Flex>
+        {/* right side content */}
+        <Flex
+          flexDirection="column"
+          h="inherit"
           alignItems="flex-end"
           justifyContent="center"
           zIndex={2}
@@ -65,15 +78,16 @@ export const Footer = () => {
             maxW="600px"
             templateColumns="1fr 1fr 1fr"
             gridTemplateRows="1fr 1fr"
-            columnGap={6}
-            rowGap={4}
+            columnGap={10}
+            rowGap={8}
+            p={4}
           >
             {footerLinkIcons.map((item) => (
               <Link
                 key={item.src}
                 href={item.to}
-                w={isTiny ? "40px" : "60px"}
-                h={isTiny ? "40px" : "60px"}
+                w={isTiny ? "40px" : "55px"}
+                h={isTiny ? "40px" : "55px"}
                 display="flex"
                 alignItems="center"
                 justifyContent="center"
@@ -92,68 +106,67 @@ export const Footer = () => {
               </Link>
             ))}
           </Grid>
-          <ChakraLink
-            as={RouterLink}
-            to="/app"
-            _hover={{ textDecoration: "none" }}
-          >
-            <Text
-              mt={{ base: 6, lg: 12 }}
-              color="blue.400"
-              w="100%"
-              fontFamily="Magistral"
-              fontSize={{ base: "md", md: "lg", lg: "xl" }}
-              textAlign={{ base: "center", md: "right" }}
-              _hover={{
-                color: "blue.200",
-                cursor: "pointer"
-              }}
-            >
-              Download the app
-            </Text>
-          </ChakraLink>
-          <ChakraLink
-            as={RouterLink}
-            to="/profile"
-            _hover={{ textDecoration: "none" }}
-          >
-            <Text
-              color="blue.400"
-              w="100%"
-              fontFamily="Magistral"
-              fontSize={{ base: "md", md: "lg", lg: "xl" }}
-              textAlign={{ base: "center", md: "right" }}
-              _hover={{
-                color: "blue.200",
-                cursor: "pointer"
-              }}
-            >
-              Go to profile and QR code
-            </Text>
-          </ChakraLink>
           <Text
-            color="white"
+            color="#FFFFFF"
             w="100%"
-            fontFamily="Magistral"
-            fontSize={{ base: "md", md: "lg", lg: "xl" }}
+            fontFamily="Inter"
+            fontSize={{ base: "md", md: "lg" }}
             textAlign={{ base: "center", md: "right" }}
+            letterSpacing="wider"
+            mt={2}
+            mb={8}
+            pr={4}
           >
-            © 2025 by Reflections | Projections
+            Download the R|P app!
           </Text>
+          <Flex flexDir="row" justifyContent="center" gap={10}>
+            <Link
+              // href="https://apps.apple.com/us/app/r-p-2025/id6744465190"
+              isExternal
+              _hover={{ transform: "scale(1.025)" }}
+              transition="all 0.3s ease"
+            >
+              <Image
+                src="/site/footer/app_store_bubble.svg"
+                alt="Download on the App Store"
+                h={{ base: "50px", md: "70px" }}
+                w="auto"
+                filter="drop-shadow(0 4px 8px rgba(0,0,0,0.3))"
+                _hover={{
+                  filter: "drop-shadow(0 6px 12px rgba(0,0,0,0.4))"
+                }}
+              />
+            </Link>
+
+            <Link
+              // href="https://play.google.com/store/apps/details?id=com.reflectionsprojections&utm_source=na_Med"
+              isExternal
+              _hover={{ transform: "scale(1.025)" }}
+              transition="all 0.3s ease"
+            >
+              <Image
+                src="/site/footer/google_play_bubble.svg"
+                alt="Get it on Google Play"
+                h={{ base: "50px", md: "70px" }}
+                w="auto"
+                filter="drop-shadow(0 4px 8px rgba(0,0,0,0.3))"
+                _hover={{
+                  filter: "drop-shadow(0 6px 12px rgba(0,0,0,0.4))"
+                }}
+              />
+            </Link>
+          </Flex>
         </Flex>
       </Flex>
-
-      {/* Bottom bar */}
-      <Box
+      {/* image background content */}
+      <Image
+        display="block"
+        position="relative"
+        bottom={0}
+        pt={20}
+        src="/site/footer/footer_buildings.svg"
         w="100%"
-        h="54px"
-        bgGradient={`
-          repeating-conic-gradient(
-            #000 0% 25%, 
-            #2d2d2d 0% 50%
-          )
-        `}
-        bgSize="70px 40px" // size of each checker square
+        zIndex={1}
       />
     </Flex>
   );
